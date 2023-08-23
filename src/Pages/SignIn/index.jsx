790c60c811fd6d8e3f43e06006ae6605783af5b8
Victor Alvarez <vaap2099@gmail.com>
2023-08-23 09:25:01 -0600

    Sign up

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import Layout from '../../Components/Layout'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { ShoppingCartContext } from '../../Context'
 
 function SignIn() {
@@ -15,9 +15,8 @@ function SignIn() {
   const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 
-  return (
-    <Layout>
-      <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
+  const renderLogin = () => {
+    return (
       <div className='flex flex-col w-80'>
         <p>
           <span className='font-light text-sm'>Email: </span>
@@ -41,10 +40,19 @@ function SignIn() {
         </div>
         <button 
         className='border border-black disabled:text-black/40 disabled:border-black/40'
+        onClick={() => setView('create-user-info')}
         disabled={hasUserAnAccount}>
           Sign Up
         </button>
       </div>
+    )
+  }
+
+  return (
+    <Layout>
+      <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
+      {renderLogin()}
+      
     </Layout>
   )
 }
